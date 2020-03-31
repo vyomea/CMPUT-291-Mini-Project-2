3fc37ae3f2c6a70922a831d9bb5c308d6029780a
Vyome Agarwal <43073294+vyomea@users.noreply.github.com>
2020-03-30 20:36:15 -0600

    bash compiler at top

--- build_index.sh
+++ build_index.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sort review.txt -u | perl break.pl | db_load -c -T -t hash rw.idx
 sort pterms.txt -u | perl break.pl | db_load -c -T -t btree pt.idx
 sort rterms.txt -u | perl break.pl | db_load -c -T -t btree rt.idx
