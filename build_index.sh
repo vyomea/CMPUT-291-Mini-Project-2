cb6661de9b945b7beea0ccb6511f1f12ef8bc1d3
Vyome Agarwal <43073294+vyomea@users.noreply.github.com>
2020-03-30 18:07:33 -0600

    Phase 1, building index

---
+++ build_index.sh
@@ -0,0 +1,4 @@
+sort review.txt -u | perl break.pl | db_load -c -T -t hash rw.idx
+sort pterms.txt -u | perl break.pl | db_load -c -T -t btree pt.idx
+sort rterms.txt -u | perl break.pl | db_load -c -T -t btree rt.idx
+sort scores.txt -u | perl break.pl | db_load -c -T -t btree sc.idx
