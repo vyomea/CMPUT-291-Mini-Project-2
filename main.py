19123ad23b5af0ad5e9223fc4e3bfea61e9f3966
Vyome Agarwal <43073294+vyomea@users.noreply.github.com>
2020-03-31 17:47:13 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+def main():
+    output_brief = False
+    output_full = False
+
+    #Print the welcome message:
+    print("Welcome to this searcher!")
+    print("To quit, please enter :quit")
+
+    quit = False
+
+    while not quit:
+        query = input(">>> ").lower()
+
+        if query == ":quit":
+            print("Thank you for using our service")
+
+        elif query == "output=breif":
+            output_brief = True
+        
+        elif query == "output=full":
+            output_full = True
+
+        else:
+            pass
+
+
+
+if __name__ == "__main__":
+    main()
