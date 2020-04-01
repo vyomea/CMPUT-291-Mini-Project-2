ea5469fe2761507e2b80d721f38ff2f67399ddc7
Vyome Agarwal <43073294+vyomea@users.noreply.github.com>
2020-03-31 22:01:12 -0600

    methods for connecting and closing the databases

--- connection.py
+++ connection.py
@@ -3,6 +3,9 @@ from bsddb3 import db
 class Connection:
     
     def __init__(self):
+        """
+        Initialize the databases and setflags
+        """
 
         # Intitialize the db
         self.reviews_db = db.DB()
@@ -16,6 +19,11 @@ class Connection:
         self.pterms_db.set_flags(db.DB_DUP)
         self.rterms_db.set_flags(db.DB_DUP)
 
+    def connect(self):
+        """
+        Connect to the databases and get the cursors
+        """
+
         # Open the connections
         self.reviews_db.open("rw.idx", None, db.DB_HASH, db.DB_CREATE)
         self.scores_db.open("sc.idx", None, db.DB_BTREE, db.DB_CREATE)
@@ -27,6 +35,25 @@ class Connection:
         self.scores_cursor = self.scores_db.cursor()
         self.pterms_cursor = self.pterms_db.cursor()
         self.rterms_cursor = self.rterms_db.cursor()
+    
+    def close(self):
+        """
+        Close the connection to the curosors and databases
+        """
 
-        
+        # Close the cursors
+        self.reviews_cursor.close()
+        self.scores_cursor.close()
+        self.pterms_cursor.close()
+        self.rterms_cursor.close()
+
+        # Close the connection to the databases
+        self.reviews_db.close()
+        self.scores_db.close()
+        self.pterms_db.close()
+        self.rterms_db.close()
+
+
+
+
         
