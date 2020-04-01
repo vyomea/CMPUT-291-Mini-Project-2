7204c5a54c08f07f6bcfa686949f94d88ecda0b1
Vyome Agarwal <43073294+vyomea@users.noreply.github.com>
2020-04-01 00:29:50 -0600

    Some __init__ data

---
+++ parser.py
@@ -0,0 +1,12 @@
+class Parser:
+
+    def __init__(self):
+
+        self.date_greater = False
+        self.date_lower = False
+
+        self.price_greater = False
+        self.price_lower = False
+
+        self.score_greater = False
+        self.score_lower = False 
