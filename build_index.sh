d538cecfa1226b5d46b0a32c631c5f281591fbe5
Vyome Agarwal <43073294+vyomea@users.noreply.github.com>
2020-03-31 21:09:30 -0600

    Fixed the file name

--- build_index.sh
+++ build_index.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sort review.txt -u | perl break.pl | db_load -T -t hash rw.idx
+sort reviews.txt -u | perl break.pl | db_load -T -t hash rw.idx
 sort pterms.txt -u | perl break.pl | db_load -c duplicates=1 -T -t btree pt.idx
 sort rterms.txt -u | perl break.pl | db_load -c duplicates=1 -T -t btree rt.idx
 sort scores.txt -u | perl break.pl | db_load -c duplicates=1 -T -t btree sc.idx
