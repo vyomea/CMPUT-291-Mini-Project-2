0a567f04e26835729f63b96fdc17bb59f41b3737
Vyome Agarwal <43073294+vyomea@users.noreply.github.com>
2020-04-01 20:37:15 -0600

    Add files via upload

--- build_index.sh
+++ build_index.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 sort reviews.txt -u | perl break.pl | db_load -T -t hash rw.idx
 sort pterms.txt -u | perl break.pl | db_load -c duplicates=1 -T -t btree pt.idx
 sort rterms.txt -u | perl break.pl | db_load -c duplicates=1 -T -t btree rt.idx
