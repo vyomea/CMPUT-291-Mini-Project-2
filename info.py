03954a7e91b631520552175e63db0021b06fc610
Vyome Agarwal <43073294+vyomea@users.noreply.github.com>
2020-04-02 10:25:50 -0600

    more query executions

--- info.py
+++ info.py
@@ -19,9 +19,9 @@ class Info:
         
         self.modFlag, self.ptermFlag, self.pterm, self.rtermFlag, self.rterm = parser.termParser(query)
 
-        #self.date_low, self.date_high = parser.dateparser(query)
+        self.date_low, self.date_high = parser.dateparser(query)
 
-       # self.price_low, self.price_high = parser.priceparser(query)
+        #self.price_low, self.price_high = parser.priceparser(query)
     
     def execute_term(self):
 
@@ -29,15 +29,22 @@ class Info:
 
         matches = set()
         other_term = None
+        product_title = None
+
+        print("THIS: {}".format(self.pterm))
+        
 
         if self.ptermFlag:
+            self.pterm = self.pterm.split()[0]
             result = self.pterms_cursor.set_range(self.pterm.encode("utf-8"))
+            
 
             while result != None:
-                product_title = result[0].decode("utf-8").split()[0]
+                product_title = result[0].decode("utf-8").lower()
+                
 
-                if len(result) > 1:
-                    other_term = result[0].decode("utf-8").split()[1]
+                # if len(result) > 1:
+                #     other_term = result[0].decode("utf-8").split()[1]
                 review_id = int(result[1].decode("utf-8"))
 
                 if self.pterm == product_title:
@@ -47,66 +54,84 @@ class Info:
             
             if len(matches) == 0:
                 exists = False
+                print("NOO")
+
+                return # TODO
         
         num = len(matches)
-        
+
+        matches_2 = set()
+        print("RTERM: {}".format(self.rterm))
         if self.rtermFlag:
+            self.rterm = self.rterm.split()[0]
             result = self.rterms_cursor.set_range(self.rterm.encode("utf-8"))
 
             while result != None:
-                product_title = result[0].decode("utf-8").split()[0]
+                review_summ_text = result[0].decode("utf-8")
                 
-                if len(result) > 1:
-                    other_term = result[0].decode("utf-8").split()[1]
+                # if len(result) > 1:
+                #     other_term = result[0].decode("utf-8").split()[1]
                 review_id = int(result[1].decode("utf-8"))
 
-                if self.rterm == product_title:
-                    matches.add(review_id)
+                if self.rterm == review_summ_text:
+                    if self.ptermFlag:
+                        matches_2.add(review_id)
+                    else:
+                        matches_2.add(review_id)
+                        matches.add(review_id)
+
 
                 result = self.rterms_cursor.next()
             
-            if len(matches) == num:
+            matches = matches.intersection(matches_2)
+
+            if len(matches) == 0:
                 exists = False
+                print("NOO2")
 
-
-        if exists:
-            self.get_data(matches)
-
+                return # TODO
         
-
-    def execute_score(self):
-        """
-        Execute the score query
-        scores.txt: (review score, review id)
-        """
-
-        exists = True
-        result = self.scores_cursor.set_range("{}".format(self.score_low).encode("utf-8"))
-         # result[0] = review score, result[1] = review ID    
-
-        matches = set()
-
-        if result != None:
-            while result != None and int(result[0].decode("utf-8")) <= self.score_high:
-                review_id = result[1].decode("utf-8")
-                review_score = result[0].decode("utf-8")
-
-                if self.ptermFlag:
-                    review_id_pterm = self.pterms_cursor.set_range(self.pterm.encode("utf-8"))[0]
-
-                    if review_id_pterm != review_id:
-                        exists = False
-
-                if self.rtermFlag:
-                    review_id_rterm = self.rterms_cursor.set_range(self.pterm.encode("utf-8"))[0]
-
-                    if review_id_rterm != review_id:
-                        exists = False
+        num = len(matches)
 
 
+        matches_2 = set()
+        # score
+        print("HIII")
+        print(self.score_low, self.score_high)
+        if self.score_low or self.score_high:
+            if not self.score_low:
+                score_low_n = 0.0
+            else:
+                score_low_n = self.score_low
 
+            if not self.score_high:
+                score_high_n = 5.0
+            else:
+                score_high_n = self.score_high
 
+            result = self.scores_cursor.set_range("{}".format(score_low_n).encode("utf-8"))
+            # result[0] = review score, result[1] = review ID
+            print("SCORE: {}".format( float(result[0].decode("utf-8"))))
+
+            while result != None:
+                review_score = float(result[0].decode("utf-8"))
+                review_id = int(result[1].decode("utf-8"))
+                
+                if review_score > float(score_low_n) and review_score < float(score_high_n):
+            
+                    if not (self.ptermFlag or self.rtermFlag):
+                        matches.add(review_id)
+                        matches_2.add(review_id)
+                    else:
+                        matches_2.add(review_id)
+                
                 result = self.scores_cursor.next()
+
+            matches = matches.intersection(matches_2)
+        print(len(matches_2))
+        print("Hello")
+        print(len(matches))
+
 
     def get_data(self, matches):
 
